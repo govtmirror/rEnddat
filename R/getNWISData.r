f8766e870c9e161cecd718bc25af1f95bd06b06b
Laura DeCicco <ldecicco@usgs.gov>
2013-01-03 13:35:45 -0600

    Fixed a bug that created an error if 2 sites were requested, but only one of them needs a particular parameter code.  Also, incorporates the ability to call different statCds.

--- R/getNWISData.r
+++ R/getNWISData.r
@@ -21,28 +21,28 @@ getNWISData <- function(totalParams,baseReturn){
       
       NWIS <-dataRequestReturn['NWIS'][[1]]
       
+      requestCombo <- unique(with(NWIS, data.frame(siteID, pCode, statCd)))
       
-      for (i in 1:length(uniquePcodes)){
-        for (j in 1:length(uniqueSites)){
-          collapse <- paste(NWISCalls[which(uniquePcodes[i] == NWIS$pCode & uniqueSites[j] == NWIS$siteID)],collapse="&")
-          NWISurl <- paste(baseReturn,collapse,sep="&")
-          cat(uniquePcodes[i],": ",system.time(
-            getEnDDaTNWISData <- read.delim(  
-              NWISurl, 
-              header = TRUE, 
-              dec=".", 
-              sep='\t',
-              fill = TRUE, 
-              comment.char="#")
-          )[3],"s. elapse\n",sep="")
-          getEnDDaTNWISData$time <- as.POSIXct(getEnDDaTNWISData$time, "%m/%d/%Y %H:%M",tz="")
-          if (1 == i){
-            NWISData <- getEnDDaTNWISData
-          } else {
-            NWISData <- mergeENDDAT(NWISData, getEnDDaTNWISData)
-          }
-        }
+      for (i in 1:nrow(requestCombo)){
+        collapse <- paste(NWISCalls[which(requestCombo$pCode[i] == NWIS$pCode & requestCombo$siteID[i] == NWIS$siteID & requestCombo$statCd[i] == NWIS$statCd)],collapse="&")
+        NWISurl <- paste(baseReturn,collapse,sep="&")
+        cat(uniquePcodes[i],": ",system.time(
+          getEnDDaTNWISData <- read.delim(  
+            NWISurl, 
+            header = TRUE, 
+            dec=".", 
+            sep='\t',
+            fill = TRUE, 
+            comment.char="#")
+        )[3],"s. elapse\n",sep="")
+        getEnDDaTNWISData$time <- as.POSIXct(getEnDDaTNWISData$time, "%m/%d/%Y %H:%M",tz="")
+        if (1 == i){
+          NWISData <- getEnDDaTNWISData
+        } else {
+          NWISData <- mergeENDDAT(NWISData, getEnDDaTNWISData)
+        }        
       }
+
     } else {
       NWISData <- NA
     }
